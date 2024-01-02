afaa8356872e2a3002ed8a236d3ee769f56a649b
Kishore-kumar-07 <bkdon54@gmail.com>
2024-01-02 21:47:34 +0530

    test1

---
+++ src/Message.tsx
@@ -0,0 +1,15 @@
+import { MouseEvent, useState } from "react";
+
+function Message(){
+    let items = ["abc","cda","los"];
+
+    const [selectedIndex,SetSelectedIndex] = useState(-1);
+
+    return <>
+     <ul>
+        
+     </ul>
+    </>;
+}
+
+export default Message;
